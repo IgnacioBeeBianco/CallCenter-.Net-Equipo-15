55727c5f9e7c3a35f3a9e0b37d93e57367eb3146
IgnacioBeeBianco <105438704+IgnacioBeeBianco@users.noreply.github.com>
2023-10-25 19:00:33 -0300

    Create CallCenter-TPC-Equipo15.sql

---
+++ CallCenter-TPC-Equipo15.sql
@@ -0,0 +1,22 @@
+/*
+Use Master
+Go
+Create Database CallCenter-TPC-Equipo15
+go
+Use CallCenter-TPC-Equipo15
+go
+
+
+Create Table Cliente() - Usuario?
+
+Create Table Administrador()
+
+Create Table Telefonista()
+
+Create Table Supervisor()
+
+Create Table Incidencias()
+
+accesos?
+
+*/
