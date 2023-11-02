3e6825d7117b6438e40b9a05ca9e5913927c535f
ManuelUsoz <miusoz5@icloud.com>
2023-11-02 20:55:21 -0300

    Creacion de clases + Fix NuGet Package

---
+++ CallCenter-Net-Equipo15/Dominio/Cuenta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Rol> Rol { get; set; }
+    }
+}
---
+++ CallCenter-Net-Equipo15/Dominio/Rol.cs
@@ -0,0 +1,8 @@
+﻿namespace Dominio
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
