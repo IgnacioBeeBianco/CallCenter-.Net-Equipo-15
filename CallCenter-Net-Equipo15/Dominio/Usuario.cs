d1707c2a43d9f581f7f31e1494bc57365b21e963
IgnacioBeeBianco <105438704+IgnacioBeeBianco@users.noreply.github.com>
2023-11-03 19:44:29 -0300

    Se crea Cliente y Usuario en Dominio

---
+++ CallCenter-Net-Equipo15/Dominio/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string DNI { get; set; }
+        public string Domicilio { get; set; }
+        public string Telefono { get; set; }
+        public char Genero { get; set; }
+        public Cuenta CuentaId { get; set; }
+    }
+}
