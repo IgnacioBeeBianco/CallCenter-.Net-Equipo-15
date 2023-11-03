95b29b6fff3ca1180ea408bca03a374ab088ff03
ManuelUsoz <miusoz5@icloud.com>
2023-11-03 20:50:17 -0300

    Rename variable en Session

--- CallCenter-Net-Equipo15/Call Center/Home.aspx.cs
+++ CallCenter-Net-Equipo15/Call Center/Home.aspx.cs
@@ -11,7 +11,7 @@ namespace Call_Center
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["username"] == null)
+            if (Session["Usuario"] == null)
             {
                 Response.Redirect("~/Login.aspx"); //Aca nos encargamos de implementar que si no logeo vaya al login
             }
