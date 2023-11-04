9cc0213213d3ad1a4e299f3e089f76826688f0f6
ManuelUsoz <miusoz5@icloud.com>
2023-11-04 20:54:55 -0300

    Display de nombre de usuario

--- CallCenter-Net-Equipo15/Call Center/Master.Master.designer.cs
+++ CallCenter-Net-Equipo15/Call Center/Master.Master.designer.cs
@@ -33,6 +33,15 @@ namespace Call_Center
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Control Username.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Username;
+
+        /// <summary>
         /// Control ContentPlaceHolder1.
         /// </summary>
         /// <remarks>
