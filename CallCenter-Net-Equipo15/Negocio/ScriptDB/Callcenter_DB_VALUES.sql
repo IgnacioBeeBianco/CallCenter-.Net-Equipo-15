0b0598e333b891cfbf980f8b51326dfbf64f5154
ManuelUsoz <miusoz5@icloud.com>
2023-11-03 21:04:01 -0300

    Update Callcenter_DB_VALUES
    Se deja script para creacion de rol admin y cuenta con rol admin

--- CallCenter-Net-Equipo15/Negocio/ScriptDB/Callcenter_DB_VALUES.sql
+++ CallCenter-Net-Equipo15/Negocio/ScriptDB/Callcenter_DB_VALUES.sql
@@ -5,3 +5,9 @@ GO
 INSERT INTO TipoIncidencia(nombre) VALUES ('Bug')
 GO
 INSERT INTO TipoIncidencia(nombre) VALUES ('Requerimiento')
+GO
+INSERT INTO Rol (nombre) VALUES ('Administrador');
+GO
+INSERT INTO Cuenta (email, password_, id_rol) VALUES ('admin@gmail.com', 'admin', (
+    SELECT id FROM Rol WHERE nombre = 'Administrador'
+))
