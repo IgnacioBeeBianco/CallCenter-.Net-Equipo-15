a15cd34d1724f0ec03462a22309b00fe6f850d79
ManuelUsoz <miusoz5@icloud.com>
2023-11-13 19:19:26 -0300

    Creacion de entidad Incidencia
    Con su respectivo Negocio y DAO

---
+++ CallCenter-Net-Equipo15/Dominio/Incidencia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Incidencia
+    {
+        public int Id { get; set; }
+        public Usuario Creador { get; set; }
+        public Usuario Asignado { get; set; }
+        public Estado Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaCierre { get; set; }
+        public string ComentarioCierre { get; set; }
+        public Prioridad Prioridad { get; set; }
+        public TipoIncidencia TipoIncidencia { get; set; }
+
+    }
+}
