dc553a4fc92ab3151bf70b80af81604a59b61b93
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-11-13 23:17:23 -0300

    Incidencia objeto creado

--- CallCenter-Net-Equipo15/Dominio/Incidencia.cs
+++ CallCenter-Net-Equipo15/Dominio/Incidencia.cs
@@ -8,13 +8,14 @@ namespace Dominio
 {
     public class Incidencia
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public Usuario Creador { get; set; }
         public Usuario Asignado { get; set; }
         public Estado Estado { get; set; }
         public DateTime FechaCreacion { get; set; }
         public DateTime FechaCierre { get; set; }
         public string ComentarioCierre { get; set; }
+        public string problematica { get; set; }
         public Prioridad Prioridad { get; set; }
         public TipoIncidencia TipoIncidencia { get; set; }
 
